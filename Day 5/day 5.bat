989ba9ef59b4535c303fd900f8c9f5ef8e95d888
akhilaambati <ambatiakhila79@gmail.com>
2024-08-22 20:15:39 +0530

    html

---
+++ Day 5/day 5.bat
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>vscode</title>
+    </head>
+    <body>
+        <h1> About Visual Studio</h1>
+        <br>
+        <p>It is an ide-Integrated development environment</p>
+        It is an software application that provides comprehensive <br>features to programmer and the developer for developing application or websites</p>
+    </body>
+</html>
